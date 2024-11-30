264455e824893b636810f3295522c9cb47c864cb
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-30 16:11:48 +0100

    implemented the testimonial section

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import './App.css';
 import { About } from './components/About';
 import { Header } from './components/Header';
 import Projects from './components/Projects';
-
+import Testimonials from './components/Testimonials';
 function App() {
   return (
     <div className="w-full overflow-hidden">
       <Header />
       <About />
       <Projects />
+      <Testimonials />
     </div>
   );
 }
