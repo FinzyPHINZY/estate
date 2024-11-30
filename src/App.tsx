553e7e0a412958720cafa204815b20f5b1332ced
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-30 15:23:36 +0100

    implemted the about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { About } from './components/About';
 import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="w-full overflow-hidden">
       <Header />
+      <About />
     </div>
   );
 }
