c505e5e16933d86ca6728ca36f1fc5c875627e55
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-30 17:05:07 +0100

    implemented footer section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { About } from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import { Header } from './components/Header';
 import Projects from './components/Projects';
 import Testimonials from './components/Testimonials';
@@ -16,6 +17,7 @@ function App() {
       <Projects />
       <Testimonials />
       <Contact />
+      <Footer />
     </div>
   );
 }
